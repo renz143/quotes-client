8dd64339b8b3c62a807e32619f24981bb52bfc92
renz143 <32805691+renz143@users.noreply.github.com>
2023-06-02 10:02:12 +0800

    modified facebook profile link

--- src/components/CreditBox.jsx
+++ src/components/CreditBox.jsx
@@ -1,6 +1,6 @@
 export const CreditBox = () => (
   <address className="my-2 fs-6 text-white">
     Created with <i className="text-danger fa fa-heart"></i> by
-    <a href="https://web.facebook.com/R3nZ314/" className="ms-2 text-reset text-decoration-underline" rel="author">Renz</a>
+    <a href="https://www.facebook.com/renz823" className="ms-2 text-reset text-decoration-underline" rel="author">Renz</a>
   </address>
 );
