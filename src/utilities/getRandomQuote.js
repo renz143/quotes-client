fe0ba1fa048f5a7d00f7e0083864d265cf4f47ac
renz143 <whilcerenzmacaambac@gmail.com>
2025-05-28 17:07:09 +0800

    fix: fetches API key from environment variables for improved security. The previously hardcoded API key has been revoked

--- src/utilities/getRandomQuote.js
+++ src/utilities/getRandomQuote.js
@@ -1,9 +1,10 @@
 import { URL } from "../config/URL";
 
 export async function getRandomQuote() {
+  const apiKey = process.env.REACT_APP_API_KEY;
   const response = await fetch(URL.GET_RANDOM_QUOTE, {
     headers: {
-      'X-Api-Key': 'iV1DmB9wmFFIlG8slEjcDA==COVxwAjUILxsQCyG'
+      'X-Api-Key': apiKey
     }
   });
   const data = await response.json();
